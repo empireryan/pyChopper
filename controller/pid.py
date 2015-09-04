0b5fa1b71f35ae52c6b9d4854230ed95cb1fd530
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-09-04 16:38:42 -0700

    Updated PID controller

--- controller/pid.py
+++ controller/pid.py
@@ -4,59 +4,72 @@ class PID(object):
     Discrete PID control
     """
 
-    def __init__(self, gains=0, derivator=0, integrator=0, integrator_max=500, 
-                 integrator_min=-500, set_point_max=1, set_point_min=-1):
+    def __init__(self, gains=0, set_point_max=1, set_point_min=-1, saturate_max = None, saturate_min = None):
 
         self.Kp = 0
         self.Ki = 0
         self.Kd = 0
-        self.integrator_max = integrator_max
-        self.integrator_min = integrator_min        
-        self.derivator = derivator
-        self.integrator = integrator
-        self.P_value = 0
-        self.I_value = 0
-        self.D_value = 0
+        self._Ti = self.Kp / self.Ki
+        self._Td = self.Kd / self.Kp
+
         self.set_point_max = set_point_max
         self.set_point_min = set_point_min
         self.set_point = 0
-        self.error = 0.0
+        self.saturate_max = saturate_max
+        self.saturate_min = saturate_min
 
-    def update(self, current_value):
+        self.ut = 0.0
+        self.ut_1 = 0.0
+        self.et = 0.0
+        self.et_1 = 0.0
+        self.et_2 = 0.0
+
+    def update(self, feedback):
         """
         Calculate PID output value for given reference input and feedback
-        :param current_value:
+        :param feedback: state of the plant
         """
+        self.et = self.set_point - feedback
+        self.ut = self.ut_1 + self.Kp * (
+            (1 + self.dt / self._Ti + self._Td / self.dt) * self.et - (1 + 2 * self._Td / self.dt) * self.et_1
+            + (self._Td * self.et_2) / self.dt)
 
-        self.error = self.set_point - current_value
-        self.P_value = self.Kp * self.error
-        self.D_value = self.Kd * (self.error - self.Derivator)
-        self.Derivator = self.error
-        self.Integrator += self.error
-        self.I_value = self.Integrator * self.Ki
-        pid = self.P_value + self.I_value + self.D_value
-        return pid
+        self.et_2 = self.et_1
+        self.et_1 = self.et
+        self.ut_1 = self.ut
 
-    def set_kp(self, P):
+        return self.ut
+
+    @property
+    def Kp(self, Kp):
+        return self._Kp
+
+    @Kp.setter
+    def Kp(self, p):
         """
 
         :param P:
         """
-        self.Kp = P
+        self._Kp = p
+        self._Ti = self._Kp / self.Ki
+        self._Td = self.Kd / self._Kp
 
-    def set_ki(self, I):
-        """
+    @property
+    def Ki(self, i):
+        return self._Ki
 
-        :param I:
-        """
-        self.Ki = I
 
-    def set_kd(self, D):
-        """
+    @Ki.setter
+    def Ki(self, i):
+        self._Ki = i
+        self._Ti = self.Kp / self._Ki
+        self._Td = self.Kd / self.Kp
 
-        :param D:
-        """
-        self.Kd = D
+    @property
+    def Kd(self, d):
+        self._Kd = d
+        self._Ti = self.Kp / self.Ki
+        self._Td = self._Kd / self.Kp
 
     @property
     def set_point(self):
@@ -64,47 +77,27 @@ class PID(object):
 
     @set_point.setter
     def set_point(self, set_point):
-        """
-        Initilize the setpoint of PID
-        :param set_point:
-        """
         if set_point > self.set_point_max:
             self._set_point = self.set_point_max
         elif set_point < self.set_point_min:
             self._set_point = self.set_point_min
         else:
             self._set_point = set_point
-        self.Integrator = 0
-        self.Derivator = 0
+
+        # @todo: ask the professor about this
+        self.ut = 0
+        self.ut_1 = 0
 
     @property
-    def integrator(self):
-        return self._integrator
+    def ut(self):
+        return self._ut
 
-    @integrator.setter
-    def integrator(self, integrator):
-        """
+    @ut.setter
+    def ut(self, ut):
+        self._ut = ut
 
-        :param Integrator:
-        """
-        if integrator > self.integrator_max:
-            self._integrator = self.integrator_max
-        elif integrator < self.integrator_min:
-            self._integrator = self.integrator_min
-        else:
-            self._integrator = integrator
-
-            
-    @property
-    def derivator(self):
-        return self._derivator
-
-    @derivator.setter
-    def derivator(self, derivator):
-        """
-
-        :param Derivator:
-        """
-        self._derivator = derivator
-
-
+        if self.saturate_max or self.saturate_min:
+            if self.saturate_max and ut > self.saturate_max:
+                self._ut = self.saturate_max
+            elif self.saturate_min and ut < self.saturate_min:
+                self._ut = self.saturate_min
